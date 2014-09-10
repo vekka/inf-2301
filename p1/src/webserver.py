39cb3c26c7201a383ede8438200ca2c5b488d1d0
kelsebo <kristian@keltux.com>
2014-09-10 13:27:00 +0200

    Added more info on webserver.printinfo

--- p1/src/webserver.py
+++ p1/src/webserver.py
@@ -7,10 +7,12 @@ class WebServer():
 	def __init__(self, port=8080):
 		self.address = "0.0.0.0"
 		self.port = str(port)
-		
+	
 	def printInfo(self):
-		print "Address = '" + self.address + ":" + self.port + "'"
-
+		print "WebServer is set up with the following config:"
+		print "- Address = '" + self.address + ":" + self.port + "'"
+	
+	
 
 
 if __name__ == '__main__':
