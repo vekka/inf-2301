b5eeaa7402ab4c5315ee539e974ad798dd8355b4
Vegard Karlsen <vegk85@hotmail.com>
2014-09-10 13:51:25 +0200

    listen lagt tell

--- p1/src/webserver.py
+++ p1/src/webserver.py
@@ -6,15 +6,30 @@ import sys
 class WebServer():
 	def __init__(self, port=8080):
 		self.address = "0.0.0.0"
-		self.port = str(port)
+		self.port = port
 	
 	def printInfo(self):
 		print "WebServer is set up with the following config:"
-		print "- Address = '" + self.address + ":" + self.port + "'"
+		print "- Address = '" + self.address + ":" + str(self.port) + "'"
 	
-	
+	def listen(self):
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.bind((self.address, self.port))
+			s.listen(5)
+		except socket.error:
+			print "Failed to create socket"
+			sys.exit()
+
+		print "Starting accept-loop"
+		while True:
+			conn, address = s.accept()
+			print "Incoming connection: "
+			print "  Conn   : ", conn
+			print "  Address: ", address
 
 
 if __name__ == '__main__':
 	ws = WebServer();
 	ws.printInfo()
+	ws.listen()
