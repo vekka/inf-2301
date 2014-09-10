f93dbfb6a364bd4174734432e4e6e37365df5bf0
Vegard Karlsen <vegk85@hotmail.com>
2014-09-10 13:22:57 +0200

    Added dummy class for web server

---
+++ p1/src/webserver.py
@@ -0,0 +1,18 @@
+
+import socket
+import sys
+
+
+class WebServer():
+	def __init__(self, port=8080):
+		self.address = "0.0.0.0"
+		self.port = str(port)
+		
+	def printInfo(self):
+		print "Address = '" + self.address + ":" + self.port + "'"
+
+
+
+if __name__ == '__main__':
+	ws = WebServer();
+	ws.printInfo()
