e9fb6485c967c2ec407c9245021da31c964b43e1
vekka <vegk85@hotmail.com>
2014-09-11 19:29:00 +0200

    funker greit, men mangler støtte for 404 Not found

--- p1/src/webserver.py
+++ p1/src/webserver.py
@@ -38,25 +38,70 @@ class WebServer():
 		if string.find(self.request, "GET") > -1:
 			print "in handle request now"
 			self.handleGet(socket)
-		elif string.find(self.request, "POST"):
-			print "received post request"
+		elif string.find(self.request, "post") or string.find(self.request, "POST"):
+			self.handlePost(socket)
 		else:
 			print "unsupported request.."
 		
 	def handleGet(self, socket):
-		
+		response = ''
+		found = 0
 		list = string.split( self.request )
+		token = "index.html"
+		newToken = token
+		#is requested resource an html file
 		for t in list:
-			if string.find(t, ".html" ):
-				print "requesting file with name: ", t
+			if string.find(t, ".html" ) > -1:
 				token = t
-
-		socket.send( self.response )
+				found = 1
+				break
+			if string.find(t, ".txt" ) > -1:
+				token = t
+				found = 1
+				break
+		
+		if found:
+			newToken = token
+			if "/" in token[0:2]:
+				newToken = token[1:len(token)]
 			
-				
+			
+			print "requesting file with name: ", newToken
+			file = open( newToken, "r" )
+			reponse = "200 OK"
+			
+			body = file.read( self.maxMessageSize )
+			response += body
+			socket.send( response )
+			file.close()
+	def handlePost(self, socket ):
+		response = ''
+		body = "this should be the body to post"
+		file = None
+		token = newToken = None
+		bodyLen = 0
+		list = string.split( self.request )
+		#is requested resource an html file
+		for t in list:
+			if string.find(t, ".html" ) > -1:
+				print t
+				token = t
+				break
+			if string.find(t, ".txt" ) > -1:
+				print t
+				token = t
+				break
 		
-		
-		
+		if "/" in token[0:2]:
+			newToken = token[1:len(token)]
+		else:
+			newToken = token
+		if newToken:
+			file = open( newToken, "w" )
+			# FIXME the POST request body should be written to the file here
+			file.write( body )
+			socket.send( body )
+			file.close()
 
 
 if __name__ == '__main__':
