cd86d84744cceb05a115ac5d24421d03b0d87cc8
vekka <vegk85@hotmail.com>
2014-09-20 15:56:16 +0200

    endret fra ctr mode til CBC mode

--- p2/src/client.py
+++ p2/src/client.py
@@ -2,7 +2,7 @@
 import socket 
 import sys
 import os
-import Crypto.Util.Counter
+import string
 from Crypto.Cipher import AES
 from Crypto import Random
 import base64
@@ -12,9 +12,7 @@ class Client():
 		self.address = address
 		self.port = port
 		self.disk = "client_disk"
-		
 		self.fileData = ""
-	
 		self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
 		
 	if len(sys.argv) > 1:
@@ -23,16 +21,23 @@ class Client():
 	else:
 		print "No server address supplied, tries connecting to default localhost:50000"
 		
+	def addPadding(self ):
+		while (len( self.fileData ) % 16) != 0:
+			self.fileData += ' ' * ( 16 - len(self.fileData) % 16 )
 	
 	#received key from server, use it to decrypt file data
 	def DecryptFileData(self, ciphertext  ):
+		#extract portion of ciphertext to find init. vector used in decryption
 		iv = ciphertext[0:16]
 		
-		print "IV size in client = ", len(iv)
+		#print "IV size in client = ", len(iv)
+		
+		#create aes object to decrypt the ciphertext
 		self.decobj = AES.new( self.symkey, AES.MODE_CBC, iv )
 		plaintext = self.decobj.decrypt( ciphertext[16:] )
-		print "uhh decrypted?? : ",  plaintext
-	
+		
+		#plaintext is returned from decrypt method, but remove padding before returning it
+		
 		return plaintext
 		
 	def getFile(self):
@@ -51,10 +56,10 @@ class Client():
 		print "IV = ", ciphertext[:16]
 		print "ciphertext = ", ciphertext[16:]
 		
-		plaintext = self.DecryptFileData(ciphertext)
+		plaintext = self.DecryptFileData( ciphertext )
+		print "decrypted ciphertext = ",  plaintext
 		
-		self.writeFile( plaintext )
-			
+		self.writeFile( plaintext.rstrip() )	
 		self.socket.close()
 		
 		
@@ -68,7 +73,6 @@ class Client():
 		
 if __name__ == '__main__':
 	c = Client()
-	
 	c.getFile()
 	
 	
--- p2/src/server.py
+++ p2/src/server.py
@@ -2,19 +2,20 @@
 import socket
 import sys
 import os
+import string
 import base64
-import Crypto.Util.Counter
 from Crypto.Cipher import AES
 from Crypto import Random
 
 # Symmetric key: use AES with CBC mode
+
+# we try to create a new key and IV for every transmission
+
 class Server():
 	def __init__(self, ipaddr='0.0.0.0', port=50000):
 		self.text = "oh hello"
 		self.port = port
-		self.ipaddress = ipaddr
-		random = Random.new()
-			
+		self.ipaddress = ipaddr	
 		if len(sys.argv) > 1:
 			self.filename = os.path.join("server_disk", sys.argv[1])
 			print "file: " + self.filename
@@ -25,11 +26,14 @@ class Server():
 		#read plaintext
 		self.fileData = self.readFile()
 	
+	def removePadding( self ):
+		self.fileData.strip()
 	def addPadding(self ):
 		while (len( self.fileData ) % 16) != 0:
-			self.fileData += '-' * ( 16 - len(self.fileData) % 16 )
+			self.fileData += ' ' * ( 16 - len(self.fileData) % 16 )
 			
 	def CreateSymmetricKey(self):
+		random = Random.new()
 		#random key, 16 bytes or 128 bits. AES supports 128, 192 and 256 bits keys
 		self.symkey = random.read( AES.key_size[0] )
 			
@@ -44,15 +48,15 @@ class Server():
 		self.encobj = AES.new( self.symkey, AES.MODE_CBC, IV = iv )
 
 		#add padding to make file data a multiple of block size, 16 bytes
-		self.addPadding( )
+		self.addPadding()
 		
 		#append IV to ciphertext(that is being created)
 		self.ciphertext = iv + self.encobj.encrypt( self.fileData )
 
 	def showStatus(self):
 		print "SERVER SIDE"
-		print "key = ", self.symkey
-		print "plaintext = ", self.fileData
+		print "symkey = ", self.symkey
+		print "plaintext(padded version) = ", self.fileData
 		print "ciphertext = ", self.ciphertext
 		
 	def readFile(self):
@@ -82,7 +86,10 @@ class Server():
 			#wait until a request has been received, before encrypting data or key
 			if requestData:
 				
+				self.CreateSymmetricKey()
+				
 				cli.sendall( self.symkey )
+				
 				self.encrypt() #encrypt data from file
 				self.showStatus()
 				
